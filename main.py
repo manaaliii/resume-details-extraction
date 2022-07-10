654bbbc5af591023fee471f8f0514119a2071511
Manali <87221209+manaaliii@users.noreply.github.com>
2022-07-10 14:22:47 +0530

    Update main.py
    added comment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from sendMail import SendEmail
 from extractDetails import Extractor
 
+#project by Disha And Manali...
+
 file = input("Enter your resume file: ")
 ch = int(input(("""1. to apply for front end development
 2. to apply for back end development.
