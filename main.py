63c7448f6d29100b4bed4731c1b47abce7be6ee2
Manali <87221209+manaaliii@users.noreply.github.com>
2022-07-10 14:16:40 +0530

    Add files via upload
    upadated main sendmail

---
+++ main.py
@@ -0,0 +1,16 @@
+from sendMail import SendEmail
+from extractDetails import Extractor
+
+file = input("Enter your resume file: ")
+ch = int(input(("""1. to apply for front end development
+2. to apply for back end development.
+3. to apply for full stack development.
+Enter your choice: """)))
+
+extract = Extractor(file, ch)
+mail = extract.extractMail()
+eligible = extract.extractSkill()
+
+if eligible:
+    mailing = SendEmail(mail)
+    mailing.sendmail()
---
+++ sendMail.py
@@ -0,0 +1,31 @@
+import smtplib
+
+
+class SendEmail:
+    __sender = 'yourmail@doamin'
+    __password = 'yourpassword'
+
+    def __init__(self, reciever):
+        self.reciever = reciever
+
+    def sendmail(self):
+        port = 587
+        smtp_server = "smtp.gmail.com"
+        message = """\
+        Subject: Hi there
+
+        This message is sent from Python."""
+
+        try:
+            with smtplib.SMTP(smtp_server, port) as server:
+                server.ehlo()  # Can be omitted
+                server.starttls()
+                server.ehlo()  # Can be omitted
+                server.login(self.__sender, self.__password)
+                server.sendmail(self.__sender, self.reciever, message)
+                server.quit()
+                print("Email sent successfully!")
+
+        except Exception as ex:
+            print("Something went wrong....", ex)
+
