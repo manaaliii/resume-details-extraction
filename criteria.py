33eabb3190e80300b07a3c66dfadea6f24fb9eff
Manali <87221209+manaaliii@users.noreply.github.com>
2022-07-10 14:18:01 +0530

    Update criteria.py
    added comment to know about file

--- criteria.py
+++ criteria.py
@@ -1,3 +1,5 @@
+#criteria is the file where all the criteria are stored, required skills of clients are stored in the file
+
 mandatory = ['HTML', 'CSS', 'Java', 'Script']
 backend = ['PHP', 'Java', 'Python']
 fullStack = ['Soft', 'Skills', 'HTTP', 'HTTPS', 'Database']
